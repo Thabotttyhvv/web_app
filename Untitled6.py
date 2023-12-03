cc1564819f47acc9aaeb2d204a10b44b5fe8862b
Thabotttyhvv <149914938+Thabotttyhvv@users.noreply.github.com>
2023-12-03 15:24:41 +0200

    Update Untitled6.py

--- Untitled6.py
+++ Untitled6.py
@@ -10,7 +10,7 @@ import re
 import pandas as pd
 
 
-import train_test_split
+from sklearn.model_selection import train_test_split
 #from sklearn.feature_extraction.text import TfidfVectorizer
 #from sklearn.linear_model import LogisticRegression, DecisionTreeClassifier
 #from sklearn.metrics import accuracy_score, confusion_matrix, ConfusionMatrixDisplay
