17d9bc0ffc7d719abe9c5431f160277bc1d58704
Thabotttyhvv <149914938+Thabotttyhvv@users.noreply.github.com>
2023-12-03 14:29:42 +0200

    Update Untitled6.py

--- Untitled6.py
+++ Untitled6.py
@@ -8,12 +8,7 @@ import streamlit as st
 import numpy as np
 import re
 import pandas as pd
-from nltk.corpus import stopwords
-from nltk.stem.porter import PorterStemmer
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score
+
 
 # Load data
 news_df = pd.read_csv('train.csv')
