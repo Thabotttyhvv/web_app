529fe6b0519f40e3e48bd90cf3bc41d9d010baa9
Thabotttyhvv <149914938+Thabotttyhvv@users.noreply.github.com>
2023-12-03 15:16:43 +0200

    Update Untitled6.py

--- Untitled6.py
+++ Untitled6.py
@@ -70,5 +70,5 @@ st.subheader("Model Evaluation Results")
 # Confusion Matrix for Decision Tree Classifier
 st.subheader("Confusion Matrix for Decision Tree Classifier")
 #cm = confusion_matrix(y_test, decision_tree_model.predict(x_test))
-cm_display = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=[False, True])
-st.pyplot(cm_display.plot())
+#cm_display = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=[False, True])
+#st.pyplot(cm_display.plot())
